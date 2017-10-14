165879a2ba4ef0daae6832a829aa099437fe12ee
Sam Baek <kie827@gmail.com>
2017-10-14 18:19:59 -0400

    Add user-service

---
+++ src/app/User.ts
@@ -0,0 +1,15 @@
+export class User {
+  name: 'Bob';
+  airportDepart: {};
+  passportType: 'US';
+  flight: [{
+    gateNumber: '100',
+    departingTime: '9:00',
+    arrivingLocation: 'NYC'
+  }];
+  travelerLevel: 2;
+
+  constructor(name) {
+    this.name = name;
+  }
+}
