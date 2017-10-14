81a89eb3887c578da65c506946435c45ade6e122
EpicPi <piyushgk1@gmail.com>
2017-10-14 16:22:17 -0400

    attempt map

--- src/app/ground-to-airport/ground-to-airport.component.ts
+++ src/app/ground-to-airport/ground-to-airport.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var google: any;
 
 @Component({
   selector: 'app-ground-to-airport',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ground-to-airport.component.css']
 })
 export class GroundToAirportComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
+    navigator.geolocation.getCurrentPosition(this.showMap);
+  }
+  showMap(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const uluru = {lat: latitude, lng: longitude};
+    const map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
   }
 
 }
